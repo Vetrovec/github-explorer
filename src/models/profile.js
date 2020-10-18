b4211cbf5251a14a77c232bb49a6acfc77e3c0ee
Stepan Vetrovec <stepan.vetrovec@gmail.com>
2020-10-18 22:28:22 +0200

    Add fetchProfile to profile model effects

--- src/models/profile.js
+++ src/models/profile.js
@@ -1,4 +1,5 @@
 import { createModel } from "@rematch/core";
+import { fetchUser } from "api/github";
 
 export default createModel()({
   name: "profile",
@@ -16,4 +17,26 @@ export default createModel()({
       profile,
     }),
   },
+  effects: (dispatch) => ({
+    async fetchProfile(username) {
+      try {
+        const response = await fetchUser(username);
+        const profile = {
+          login: response.login,
+          name: response.name,
+          bio: response.bio,
+          avatarUrl: response.avatar_url,
+        };
+        dispatch.profile.SET_PROFILE(profile);
+        dispatch.profile.SET_ERROR(null);
+      } catch (e) {
+        let errorMessage = "containers.profile.errors.default";
+        if (e.message === "Not Found") {
+          errorMessage = "containers.profile.errors.notfound";
+        }
+        dispatch.profile.SET_PROFILE(null);
+        dispatch.profile.SET_ERROR(errorMessage);
+      }
+    },
+  }),
 });
