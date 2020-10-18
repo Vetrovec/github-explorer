5b92701f629cce733606d72312a3259f75426f40
Stepan Vetrovec <stepan.vetrovec@gmail.com>
2020-10-18 21:34:09 +0200

    Fix

--- src/createStore.js
+++ src/createStore.js
@@ -4,7 +4,7 @@ import immerPlugin from "@rematch/immer";
 import selectPlugin from "@rematch/select";
 
 export default function createStore() {
-  const models = [];
+  const models = {};
   const plugins = [immerPlugin(), loadingPlugin(), selectPlugin()];
   const store = Rematch.init({
     models,
