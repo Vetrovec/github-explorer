77182db5373b0520c98c4898440bb34ed046c3b3
Stepan Vetrovec <stepan.vetrovec@gmail.com>
2020-10-18 23:51:24 +0200

    Add profile model to Profile container

---
+++ src/containers/Profile/elements.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ErrorBox = styled.p`
+  position: absolute;
+  top: 32px;
+  width: 75%;
+  padding: 8px 16px;
+  border: 1px solid #fc5c54;
+  margin: 0 auto;
+  background-color: #fc5c5422;
+  border-radius: 4px;
+  color: #fc5c54;
+`;
+
+export const ProfileContainer = styled.div`
+  display: flex;
+  width: 75%;
+  max-width: 75%;
+  flex-direction: column;
+  row-gap: 12px;
+`;
--- src/containers/Profile/index.jsx
+++ src/containers/Profile/index.jsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
+import { FormattedMessage } from "react-intl";
+import { ErrorBox, ProfileContainer } from "./elements";
 import MainLayout from "layouts/Main";
+import ProfileView from "components/ProfileView";
 import UsernameForm from "components/UsernameForm";
 
 function Profile() {
+  const dispatch = useDispatch();
+  const error = useSelector((state) => state.profile.error);
+  const loading = useSelector(
+    (state) => state.loading.effects.profile.fetchProfile
+  );
+  const profile = useSelector((state) => state.profile.profile, shallowEqual);
   return (
-    <MainLayout title={"containers.profile.title"}>
-      <UsernameForm onSubmit={() => null} />
+    <MainLayout title={"containers.profile.title"} loading={loading}>
+      {error && (
+        <ErrorBox>
+          <FormattedMessage id={error} />
+        </ErrorBox>
+      )}
+      <ProfileContainer>
+        {profile ? (
+          <ProfileView
+            name={profile.name || profile.login}
+            bio={profile.bio}
+            avatarUrl={profile.avatarUrl}
+          />
+        ) : (
+          <UsernameForm
+            onSubmit={({ username }) => dispatch.profile.fetchProfile(username)}
+          />
+        )}
+      </ProfileContainer>
     </MainLayout>
   );
 }
