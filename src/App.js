406ca4bd954e16782c068270c539f7bc28dbd22b
Stepan Vetrovec <stepan.vetrovec@gmail.com>
2020-10-18 21:35:31 +0200

    Add providers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from "react";
 import "./App.css";
+import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
+import { IntlProvider } from "react-intl";
+import { Router } from "@reach/router";
+import createStore from "./createStore";
+
+const store = createStore();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <Provider store={store}>
+        <IntlProvider messages={[]} locale="en">
+          <Helmet
+            titleTemplate="GitHub Explorer - %s"
+            defaultTitle="GitHub Explorer"
+          />
+          <div className="App">
+            <Router />
+          </div>
+        </IntlProvider>
+      </Provider>
+    </HelmetProvider>
   );
 }
 
