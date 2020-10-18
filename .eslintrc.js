6709d3103722f9250d832c65e0ebefd08c7fd9cb
Stepan Vetrovec <stepan.vetrovec@gmail.com>
2020-10-18 20:31:25 +0200

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/recommended",
+  ],
+  env: {
+    es6: true,
+    node: false,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  ignorePatterns: ["/node_modules/**", "/build/**"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "import/order": ["error"],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx"],
+        moduleDirectory: ["node_modules", "src"],
+      },
+    },
+    react: {
+      version: "detect",
+    },
+  },
+};
