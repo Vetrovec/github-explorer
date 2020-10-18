45eacb1ead863c808b3d029a5b87edc52ade858c
Stepan Vetrovec <stepan.vetrovec@gmail.com>
2020-10-18 23:07:48 +0200

    Add ErrorMessage component

---
+++ src/components/ErrorMessage/elements.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Message = styled.span`
+  display: block;
+  color: #fc5c54;
+  text-align: right;
+`;
---
+++ src/components/ErrorMessage/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { FormattedMessage } from "react-intl";
+import { Message } from "./elements";
+
+function ErrorMessage({ children }) {
+  return (
+    <Message>
+      <FormattedMessage
+        id={children.toString()}
+        defaultMessage="Unknown error"
+      />
+    </Message>
+  );
+}
+
+ErrorMessage.propTypes = {
+  children: PropTypes.string.isRequired,
+};
+
+export default ErrorMessage;
