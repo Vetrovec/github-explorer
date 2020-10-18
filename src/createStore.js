60b50c810cfd0b7b67cdbe291ab51b284212f2b6
Stepan Vetrovec <stepan.vetrovec@gmail.com>
2020-10-18 21:46:15 +0200

    Add profile model to store

--- src/createStore.js
+++ src/createStore.js
@@ -2,9 +2,9 @@ import * as Rematch from "@rematch/core";
 import loadingPlugin from "@rematch/loading";
 import immerPlugin from "@rematch/immer";
 import selectPlugin from "@rematch/select";
+import models from "models";
 
 export default function createStore() {
-  const models = {};
   const plugins = [immerPlugin(), loadingPlugin(), selectPlugin()];
   const store = Rematch.init({
     models,
