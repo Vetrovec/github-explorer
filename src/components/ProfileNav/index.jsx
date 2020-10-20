3e2500751ece32f29874b3d9f54c961d282765e6
Stepan Vetrovec <stepan.vetrovec@gmail.com>
2020-10-20 04:13:47 +0200

    Add intl to ProfileNav component

--- src/components/ProfileNav/index.jsx
+++ src/components/ProfileNav/index.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FormattedMessage } from "react-intl";
 import { Bar } from "./elements";
 import { Button } from "components/Inputs";
 
 function ProfileNav({ onClose, onOpen }) {
   return (
     <Bar>
-      <Button onClick={() => onClose()}>Close</Button>
-      <Button onClick={() => onOpen()}>Open</Button>
+      <Button onClick={() => onClose()}>
+        <FormattedMessage id="components.profilenav.inputs.close" />
+      </Button>
+      <Button onClick={() => onOpen()}>
+        <FormattedMessage id="components.profilenav.inputs.open" />
+      </Button>
     </Bar>
   );
 }
