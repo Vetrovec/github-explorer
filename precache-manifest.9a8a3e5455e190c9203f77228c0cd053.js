14e89fb10b79430908ede43fba9c14757bf95587
Stepan Vetrovec <stepan.vetrovec@gmail.com>
2020-10-21 12:49:56 +0200

    Updates

---
+++ precache-manifest.9a8a3e5455e190c9203f77228c0cd053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a6f912d653e1d74498219f0d19bdaf",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "b8ff45057154ef21a30a",
+    "url": "/github-explorer/static/css/main.1ca85877.chunk.css"
+  },
+  {
+    "revision": "647461daae5d88b84a93",
+    "url": "/github-explorer/static/js/2.ee7462de.chunk.js"
+  },
+  {
+    "revision": "7dbfd7ba214f947080b54129a105d9fe",
+    "url": "/github-explorer/static/js/2.ee7462de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ff45057154ef21a30a",
+    "url": "/github-explorer/static/js/main.d6e6744e.chunk.js"
+  },
+  {
+    "revision": "b76f610d1055037be066",
+    "url": "/github-explorer/static/js/runtime-main.efd11135.js"
+  }
+]);
